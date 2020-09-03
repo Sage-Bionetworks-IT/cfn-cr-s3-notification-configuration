377a33a181cdf07efd7d6c7c29072e32ab6fd0f4
Khai Do <zaro0508@gmail.com>
2020-09-03 10:05:27 -0700

    fix typo and s3 reference

--- lambda_configuration/app.py
+++ lambda_configuration/app.py
@@ -14,22 +14,22 @@ def _get_comma_delimited_list(event, parameter):
 def get_parameters(event):
     notification_bucket = event['ResourceProperties']['NotificationBucket']
     lambda_function_arn = event['ResourceProperties']['LambdaFunctionArn']
-    labda_notification_events = _get_comma_delimited_list(event, 'LambdaNotificationEvents')
-    return notification_bucket, lambda_function_arn, labda_notification_events
+    lambda_notification_events = _get_comma_delimited_list(event, 'LambdaNotificationEvents')
+    return notification_bucket, lambda_function_arn, lambda_notification_events
 
 try:
     s3 = boto3.client("s3")
 except Exception as e:
     helper.init_failure(e)
 
-def add_notification(notification_bucket, lambda_function_arn, labda_notification_events):
+def add_notification(notification_bucket, lambda_function_arn, lambda_notification_events):
     bucket_notification = s3.BucketNotification(notification_bucket)
     response = bucket_notification.put(
         NotificationConfiguration={
             'LambdaFunctionConfigurations': [
                 {
                     'LambdaFunctionArn': lambda_function_arn,
-                    'Events': labda_notification_events
+                    'Events': lambda_notification_events
                 }
             ]
         }
